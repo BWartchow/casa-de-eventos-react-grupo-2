8318b6a803e73aaf0f78cff1add337346230ae63
Wartchow <be.wartchow@sap.com>
2024-02-22 15:49:08 -0300

    Validação de login

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,7 +1,34 @@
-function Login() {
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const DoTheLogin = async () => {
+        try {
+            const response = await axios.get('http://localhost:3000/users');
+            const users = response.data;
+            const userFound = users.find(user => user.name === name && user.password === password);
+            if (userFound) {
+                navigate('/homepage');
+            } else {
+                setError('Nome ou senha inválido');
+            }
+        } catch (error) {
+            console.error('Erro ao buscar eventos', error);
+        }
+    };
     return (
-        <div className="login">
-            <h1>Formulário de login aqui</h1>
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Nome" value={name} onChange={e => setName(e.target.value)} />
+            <input type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)} />
+            <button onClick={DoTheLogin}>Entrar</button>
+            {error && <p>{error}</p>}
         </div>
     )
 }
