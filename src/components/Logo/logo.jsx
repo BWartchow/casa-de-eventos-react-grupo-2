07ae9795ca23b2db3129d867543fc2200589a8a7
Júlia Küchle <juliagkuchle@gmail.com>
2024-02-23 14:57:19 -0300

    fix bug da referencia do logo

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -1,10 +1,13 @@
 import { LogoContainer, LogoImage } from "./logo.styles"
 
 function Logo() {
-    return (
-        <LogoContainer>
-            <LogoImage src="./images/logo.svg" alt="Logomarca da casa de eventos" />
-        </LogoContainer>
-    )
+	return (
+		<LogoContainer>
+			<LogoImage
+				src="/images/logo.svg"
+				alt="Logomarca da casa de eventos"
+			/>
+		</LogoContainer>
+	)
 }
 export default Logo
