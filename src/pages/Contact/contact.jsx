1e37a9a000d2bc09445611c93532e5010988ae6b
Nicolas-hkarling <nicolasherique8@gmail.com>
2024-02-26 00:38:54 -0300

    pagina de contato

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -1,18 +1,71 @@
-import { Container, Title } from './contact.styles'
+import { Container, Title, FormInput, SendBtn } from './contact.styles'
+import { useState } from "react"
+import axios from "axios"
+import Cabecalho from '../../components/Header/header'
+import Rodape from '../../components/Footer/footer'
+function Contato () {
+    // AQUI ENTRARIAM AS FUNÇÕES JAVASCRIPT, HOOKS, ETC...
+    const [contact, setContact]= useState({
+        name: "",
+        email:"",
+        message:"",
+    })
+    const [error, setError] = useState("")
 
-import Header from '../../components/Header/header'
-import Footer from '../../components/Footer/footer'
+    const handleChange = (e) => {
+		const { name, value } = e.target;
+		setContact((prevContact) => ({
+			...prevContact,
+			[name]: value,
+		}));
+	}
+    const contactSender = async () => {
+        try{
+            if(contact.email.includes("@") && !(contact.name==null)){
+              const response = await axios.post("http://localhost:3000/contacts", contact) 
+            } else{
+                setError("O email deve ser valido e o nome nao pode estar vazio");
+            }
+        } catch (error){
+            alert(error)
 
-function Contact () {
+        }
+    }
+
+    
+
     return (
+        // AQUI EU COLOCO TUDO QUE EU QUER EXIBIR NA TELA
         <>
-            <Header />
+            <Cabecalho />
             <Container>
                 <Title>Página de Contato</Title>
+                <FormInput
+				type="text"
+                name = 'name'
+				placeholder="Nome"
+				value={contact.name}
+				onChange={handleChange}
+			/>
+            <FormInput
+				type="text"
+				placeholder="Email"
+                name = 'email'
+				value={contact.email}
+				onChange={handleChange}
+			/>    
+            <FormInput
+				type="text"
+				placeholder="Mensagem"
+                name = 'message'
+				value={contact.message}
+				onChange={handleChange}
+			/>
+            <SendBtn onClick = {contactSender}>enviar</SendBtn>
             </Container>
-            <Footer />
+            <Rodape />
         </>  
     )
 }
 
-export default Contact
+export default Contato
--- src/pages/Contact/contact.styles.jsx
+++ src/pages/Contact/contact.styles.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 1140px;
-    min-height: calc(100vh - 240px);
-    display: block;
-    margin: auto;
+margin: 30px 120px;
+padding: 30px 0;
+-webkit-box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
+-moz-box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
+box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
+display: flex;
+flex-direction: column;
+align-items: center;
+background: white;
 `
 export const Title = styled.h1`
     font-family: 'Playfair Display', serif;
@@ -12,4 +17,24 @@ export const Title = styled.h1`
     font-size: 36px;
     font-weight: 400;
     color: #1F0049;
+
 `
+export const FormInput = styled.input`
+	margin: 8px 0;
+	width: 60%;
+	padding: 12px 20px;
+	margin: 8px 0;
+	box-sizing: border-box;
+	&:focus {
+		border: 3px solid #1f0049;
+	}
+`
+
+export const SendBtn = styled.button`
+	width: 60%;
+	margin: 10px 0 10px 0;
+	height: 36px;
+	background: #1f0049;
+	color: #ffffff;
+	text-align: center;
+	border-radius: 18px;`
